f8376a3c6697c54b28f35ce86d93d6f800fb957c
Enrique Mendez <eqm@mit.edu>
2023-08-27 09:20:32 -0700

    Update main.py to resize with parent window width

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
-from PyQt6 import QtWidgets
+from PyQt6 import QtWidgets, QtGui
 from PyQt6.QtWidgets import QApplication, QMainWindow
+from widgets import AutoExpandingTextEdit, InputBox
 import sys
 
 
 def main():
-	app = QApplication(sys.argv)
-	win = QMainWindow()
-	win.setGeometry(200, 200, 300, 300)
-	win.setWindowTitle("My first app")
-	label = QtWidgets.QLabel(win)
-	label.setText("Hello world!")
-	label.move(50, 50)
-	win.show()
-	sys.exit(app.exec())
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(0, 0, 748, 440)
+    win.setWindowTitle("My first app")
+
+    # Create a central QWidget and set it to the main window
+    central_widget = QtWidgets.QWidget()
+    win.setCentralWidget(central_widget)
+
+    # Create a QVBoxLayout and set it to the central widget
+    vlayout = QtWidgets.QVBoxLayout()
+    central_widget.setLayout(vlayout)
+
+    # Create and set the InputBox
+    input_box = InputBox()
+    vlayout.addWidget(input_box)
+
+    # Create and set the AutoExpandingTextEdit
+    text_edit = AutoExpandingTextEdit()
+    text_edit.setPlaceholderText("Type a message...")
+    text_edit.setFixedHeight(50)
+    vlayout.addWidget(text_edit)
+
+    win.show()
+    sys.exit(app.exec())
+
 
 
 if __name__ == "__main__":
+	print("#####Main Start#####")
 	main()
